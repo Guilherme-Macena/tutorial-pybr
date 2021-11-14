52cb5239aef09ad1dd6e73ab457e47ab96f6ef17
Guilherme Macena <macenaguilherme1@gmail.com>
2021-11-14 15:27:36 -0300

    Adicionando documentação ao projeto

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -1,18 +1,74 @@
-from typing import List
+from typing import ItemsView, List
 from fastapi import FastAPI, Depends
 from uuid import UUID
-from api_pedidos.esquema import Item
+from starlette.responses import HTMLResponse
+
+from starlette.status import HTTP_100_CONTINUE
+from api_pedidos.esquema import ErroResponse, Item, HealthCheckResponse
 from api_pedidos.excecao import PedidoNaoEncontradoError, FalhaDeComunicacaoError
 from fastapi import FastAPI, Depends, Request
 from fastapi.responses import JSONResponse
 from http import HTTPStatus
 
+import os
+import httpx
+
 
 app = FastAPI()
 
 
+# def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> List[Item]:
+# pass
+
+
+# tenant e apikey fixos somente para demonstrações
+APIKEY = os.environ.get("APIKEY", "5734143a-595d-405d-9c97-6c198537108f")
+TENANT_ID = os.environ.get("TENANT_ID", "21fea73c-e244-497a-8540-be0d3c583596")
+MAGALU_API_URL = "http://127.0.0.1:8080"
+MAESTRO_SERVICE_URL = f"{MAGALU_API_URL}/maestro/v1"
+
+
+def _recuperar_itens_por_pacote(uuid_do_pedido, uuid_do_pacote):
+    response = httpx.get(f"{MAESTRO_SERVICE_URL}/orders/{uuid_do_pedido}/packges/{uuid_do_pacote}/items",
+                         )
+    response.raise_for_status()
+    return[
+        Item(
+            sku=item["product"]["code"],
+            # campos que utilizam a função get são opcionais
+            description=item["product"].get("description", ""),
+            image_url=item["product"].get("image_url", ""),
+            reference=item["product"].get("reference", ""),
+            quantity=item["quantity"],
+
+        )
+        for item in response.json()
+    ]
+
+
 def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> List[Item]:
-    pass
+    try:
+        response = httpx.get(f"{MAESTRO_SERVICE_URL}/orders/{identificacao_do_pedido}",
+                             headers={"X-Api-Key": APIKEY, "X-Tenant-Id": TENANT_ID},)
+        response.raise_for_status()
+        pacotes = response.json()["packges"]
+        itens = []
+        for pacote in pacotes:
+            itens.extend(_recuperar_itens_por_pacote(
+                identificacao_do_pedido, pacote["uuid"]))
+            return itens
+    except httpx.HTTPStatusError as exc:
+        # aqui poderiam ser tratados outros erros como, autenticação por exemplo
+        if exc.response.status_code == HTTPStatus.NOT_FOUND:
+            raise PedidoNaoEncontradoError() from exc
+        raise exc
+    except httpx.HTTPError as exc:
+        raise FalhaDeComunicacaoError() from exc
+
+
+@app.get("/")
+def read_root():
+    return "PAGINA INICIAL"
 
 
 @app.exception_handler(FalhaDeComunicacaoError)
@@ -25,11 +81,20 @@ def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontrado
     return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Pedido não encontrado"})
 
 
-@app.get("/healthcheck")
+@app.get("/healthcheck", tags=["healthcheck"], summary="Integridade do sistema", description="Checa se o servidor esrá online", response_model=HealthCheckResponse)
 async def healthcheck():
-    return {"status": "ok"}
+    return HealthCheckResponse(status="ok")
 
 
-@app.get("/orders/{identificacao_do_pedido}/items")
+@app.get("/orders/{identificacao_do_pedido}/items", responses={
+    HTTPStatus.NOT_FOUND.value: {
+        "description": "Pedido não encontrado",
+        "model": ErroResponse,
+    },
+    HTTPStatus.BAD_GATEWAY.value: {
+        "description": "Falha de Comunicação com o servidor remoto",
+        "model": ErroResponse,
+    }},
+    tags=["pedidos"], summary="Itens de um pedido", description="Retorna todos os itens de um determinado pedido", response_model=List[Item])
 def listar_itens(itens: List[Item] = Depends(recuperar_itens_por_pedido)):
     return itens
